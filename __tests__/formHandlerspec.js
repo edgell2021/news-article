66ea4b8144820c62d263b0267a6562858f90f65a
edgell2021 <joedge2020@gmail.com>
2020-02-29 08:27:43 -0500

    jest test

--- __tests__/formHandlerspec.js
+++ __tests__/formHandlerspec.js
@@ -1,11 +1,3 @@
-describe("check name value", () => {
-  test("it should trigger alert on name Kirk", () => {
-    const value = "Kirk";
-    expect(value).toEqual("Kirk");
-    expect(value).not.toEqual("Lucy");
-  });
-});
-
 describe("check name called", () => {
   test("it should trigger alert on name Kirk", () => {
     const names = ["Picard", "Janeway", "Kirk", "Archer", "Georgiou"];
