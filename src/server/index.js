279fb0cc1f20813a6e8ef8c789bafac3a8d6dcd8
edgell2021 <joedge2020@gmail.com>
2020-02-29 08:58:06 -0500

    set up env  file

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,6 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 var path = require("path");
 const express = require("express");
 const mockAPIResponse = require("./mockAPI.js");
@@ -28,4 +31,10 @@ app.listen(8081, function() {
 app.get("/test", function(req, res) {
   console.log(mockAPIResponse);
   res.send(mockAPIResponse);
+});
+
+var AYLIENTextAPI = require("aylien_textapi");
+var textapi = new aylien({
+  application_id: process.env.API_ID,
+  application_key: process.env.API_KEY
 });
